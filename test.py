16ad54f9bb9d175e168c581217ce81bdf6b7aad2
Mark Melnic <markhappy200@gmail.com>
2020-09-10 15:27:23 +0200

    Added autotest

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 
-from tests import test_mobile_de
+from tests.autotest import autotest
 
 if __name__ == '__main__':
     # testing mobile_de
-    test_mobile_de.CheckMethods()
+    autotest()
---
+++ tests/autotest.py
@@ -0,0 +1,5 @@
+
+def autotest():
+    # testing mobile_de
+    from tests import test_mobile_de
+    test_mobile_de.CheckMethods()
