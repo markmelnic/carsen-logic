709dd436581df19d63080663f4c32c6388111881
Mark Melnic <markhappy200@gmail.com>
2020-09-10 13:10:39 +0200

    Modified test

--- mobile_de/test.py
+++ mobile_de/test.py
@@ -1,18 +1,9 @@
 
 import json
 
-from search import search
-
-_MAKESJSON = 'makes.json'
+from methods import search
 
 TEST_SEARCH = ['Lexus', 'IS300', '', '', '', '', '', '']
 
 if __name__ == '__main__':
     print(search(TEST_SEARCH))
-
-def load_makes(website : str) -> dict:
-    with open(_MAKESJSON, 'r', encoding="utf-8", newline='') as mjson:
-        data = mjson.read()
-        makes_dict = (json.loads(data))
-        makes_dict = makes_dict[website]
-    return makes_dict
