267b38aa7d7139df93a54f1a566850cb574b25b3
Mark Melnic <markhappy200@gmail.com>
2020-09-11 14:36:38 +0200

    Initiated mobile_de

--- mobile_de/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from inspect import getsourcefile
-import os.path
-import sys
-
-CUR_PATH = os.path.abspath(getsourcefile(lambda:0))
-CUR_DIR = os.path.dirname(CUR_PATH)
-PARENT_DIR = CUR_DIR[:CUR_DIR.rfind(os.path.sep)]
-sys.path.insert(0, PARENT_DIR)
