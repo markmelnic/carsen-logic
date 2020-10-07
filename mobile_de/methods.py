bf72402d1a9c245e2640118f06db7e466e79f226
Mark Melnic <markhappy200@gmail.com>
2020-10-07 12:21:15 +0200

    Moved makes loader to utils

--- mobile_de/methods.py
+++ mobile_de/methods.py
@@ -1,8 +1,7 @@
-import json, scalg
+import scalg
 
+from utils import load_makes
 from mobile_de.scraper import *
-from settings import _MAKESJSON, TEST_MAKESJSON
-
 
 # perform a detailed search of each listing
 def search(search_params: list) -> list:
@@ -54,20 +53,3 @@ def checker(data: list) -> list:
 
     assert changed == True
     return data
-
-
-# load makes and models from json file
-def load_makes(website: str) -> dict:
-    try:
-        with open(_MAKESJSON, "r", encoding="utf-8", newline="") as mjson:
-            data = mjson.read()
-            makes_dict = json.loads(data)
-            makes_dict = makes_dict[website]
-        return makes_dict
-
-    except FileNotFoundError:
-        with open(TEST_MAKESJSON, "r", encoding="utf-8", newline="") as mjson:
-            data = mjson.read()
-            makes_dict = json.loads(data)
-            makes_dict = makes_dict[website]
-        return makes_dict
---
+++ utils.py
@@ -0,0 +1,19 @@
+import json
+
+from settings import _MAKESJSON, TEST_MAKESJSON
+
+# load makes and models from json file
+def load_makes(website: str) -> dict:
+    try:
+        with open(_MAKESJSON, "r", encoding="utf-8", newline="") as mjson:
+            data = mjson.read()
+            makes_dict = json.loads(data)
+            makes_dict = makes_dict[website]
+        return makes_dict
+
+    except FileNotFoundError:
+        with open(TEST_MAKESJSON, "r", encoding="utf-8", newline="") as mjson:
+            data = mjson.read()
+            makes_dict = json.loads(data)
+            makes_dict = makes_dict[website]
+        return makes_dict
