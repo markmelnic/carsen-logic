93007b02ecbbc05b90ed7df0277f115d02d7122d
Mark Melnic <markhappy200@gmail.com>
2020-10-12 15:16:30 +0200

    Manual database test added

--- tests/manual_test.py
+++ tests/manual_test.py
@@ -4,12 +4,13 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
+from db.db import DB
 from mobile_de.methods import search, surface_search
 from settings import TEST_DATA_FILE, TEST_SEARCH_PARAMS
 import scalg, csv
 
 if __name__ == "__main__":
-    t = 3
+    t = 4
     if t == 1:
         with open(TEST_DATA_FILE, "r") as csvfile:
             data = list(csv.reader(csvfile))
@@ -24,3 +25,6 @@ if __name__ == "__main__":
         dataset = surface_search(TEST_SEARCH_PARAMS)
         for dt in dataset:
             print(dt[1:])
+    elif t == 4:
+        db = DB()
+        db.close_conn()
