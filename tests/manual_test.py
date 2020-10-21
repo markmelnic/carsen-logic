a465c36d3309d0803d8d3b8568d5d21620d9c5a4
Mark Melnic <markhappy200@gmail.com>
2020-10-21 14:29:02 +0200

    Added args parser for test number

--- tests/manual_test.py
+++ tests/manual_test.py
@@ -1,4 +1,5 @@
 import os, sys, inspect
+from argparse import ArgumentParser, RawTextHelpFormatter
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -10,30 +11,33 @@ from settings import TEST_DATA_FILE, TEST_SEARCH_PARAMS
 import scalg, csv
 
 if __name__ == "__main__":
-    t = 6
-    if t == 1:
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument("test_nr", metavar="test number", type=int, help="Which test would you like to run.")
+    args = parser.parse_args()
+
+    if args.test_nr == 1:
         with open(TEST_DATA_FILE, "r") as csvfile:
             data = list(csv.reader(csvfile))
         dataset = scalg.score_columns(data, [2, 3, 4], [1, 0, 0])
         for dt in dataset:
             print(dt[1:])
-    elif t == 2:
-        dataset = search(TEST_SEARCH_PARAMS)
+    elif args.test_nr == 2:
+        dataset = search(TEST_SEARCH_PARAMS, db=True)
         for dt in dataset:
             print(dt[1:])
-    elif t == 3:
+    elif args.test_nr == 3:
         dataset = surface_search(TEST_SEARCH_PARAMS)
         for dt in dataset:
             print(dt[1:])
-    elif t == 4:
+    elif args.test_nr == 4:
         db = DB()
         db.close_conn()
-    elif t == 5:
+    elif args.test_nr == 5:
         db = DB()
-        dataset = search(TEST_SEARCH_PARAMS)
+        dataset = search(TEST_SEARCH_PARAMS, db=True)
         db.add_values(dataset[0], dataset[1])
         db.close_conn()
-    elif t == 6:
+    elif args.test_nr == 6:
         with open(TEST_DATA_FILE, "r") as csvfile:
             data = list(csv.reader(csvfile))
         try:
