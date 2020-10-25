d1e6d0f1edfe1c6f135d9be301d0597f60754110
Mark Melnic <markhappy200@gmail.com>
2020-10-25 09:24:11 +0100

    Separated general tests from mobile_de

---
+++ tests/test_general.py
@@ -0,0 +1,22 @@
+import os, json, unittest
+
+from db import DB
+from settings import *
+
+# test utilities
+class Utilities(unittest.TestCase):
+    def test_makes_json(self):
+        try:
+            makes = load_makes("mobile_de")
+        except json.decoder.JSONDecodeError:
+            os.remove(_MAKES_JSON)
+        except FileNotFoundError:
+            self.skipTest("Makes json file not found.")
+        self.assertIsNot(load_makes("mobile_de"), "" or None)
+
+# test the database
+class Database(unittest.TestCase):
+    def test_db(self):
+        db = DB()
+        self.assertTrue(db)
+        db.close_conn()
